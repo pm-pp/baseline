5eb002e6441cd8c9baa4a08d46fa18a64f9589ba
Pedro Mendes <pedro.mendes@perchpeek.com>
2024-12-31 15:55:30 +0000

    First commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Things explored & learned
+
+- [Exclusive Accordion](https://developer.chrome.com/docs/css-ui/exclusive-accordion)
+  - https://codepen.io/web-dot-dev/pen/BaMbKyM
+  - https://codepen.io/web-dot-dev/pen/XWvBZNo
+- [Popover API](https://developer.mozilla.org/en-US/docs/Web/API/Popover_API)
+  - [MDN Popover API examples](https://mdn.github.io/dom-examples/popover-api/)
+  - [Chrome for Developers - 079: Popover and Dialog](https://www.youtube.com/watch?v=6u7EbMuxNkE)
+- [`<dialog>`](https://developer.mozilla.org/en-US/docs/Web/HTML/Element/dialog)
---
+++ src/App.tsx
@@ -0,0 +1,34 @@
+import { useState } from "react";
+import { Accordion } from "./Accordion";
+
+const features = [
+  { title: "Exclusive accordion", content: <Accordion /> },
+  { title: "Popover API", content: <>Popover API</> },
+];
+
+function App() {
+  const [feature, setFeature] = useState(features[0]);
+
+  return (
+    <div className="flex h-screen flex-col items-center gap-8 p-4">
+      <header className="flex flex-col gap-4">
+        <h1 className="text-3xl font-bold">Baseline 2023, 2024 & beyond</h1>
+        <nav className="flex justify-center divide-x text-lg">
+          {features.map((f) => (
+            <button
+              key={f.title}
+              className="px-4 underline-offset-4 hover:underline data-[selected=true]:underline"
+              data-selected={f.title === feature.title}
+              onClick={() => setFeature(f)}
+            >
+              {f.title}
+            </button>
+          ))}
+        </nav>
+      </header>
+      <main className="">{feature.content}</main>
+    </div>
+  );
+}
+
+export { App };
